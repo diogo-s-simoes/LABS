70d84197577bbb4a9865cd0ddf5c18a7a2e1e867
juliamestre1 <52825929+juliamestre1@users.noreply.github.com>
2021-03-11 13:30:53 +0000

    Add files via upload

---
+++ Gas.C
@@ -0,0 +1,26 @@
+#include "NewReader.h"
+#include "TGraph.h"
+#include "TCanvas.h"
+#include "TApplication.h"
+int main()  {
+
+	NewReader ICV("ICV.txt");
+	NewReader ICP("ICP.txt");
+	NewReader ICT("ICT.txt");
+
+	vector<double> instante;
+
+	vector <vector<double>> todos;
+	int Nlines = ICV.GetNrInstantes();
+
+	for(int i=0; i<Nlines; i++){
+		instante.push_back(ICV.GetTempo(i));
+		instante.push_back(ICV.GetDataVector(i));
+		instante.push_back(ICP.GetDataVector(i));
+		instante.push_back(ICT.GetDataVector(i));
+
+		todos.push_back(instante);
+	}
+
+	return 0;
+}
