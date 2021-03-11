bb53688e67d0c5fa9448b4fe3048e4fb7b4039dc
rafadays <45083066+rafadays@users.noreply.github.com>
2021-03-11 16:11:13 +0000

    Update Gas.C

--- Gas.C
+++ Gas.C
@@ -30,6 +30,45 @@ int main()  {
     LETintegral lIC(todos);
 
     cout<<lIC.integrate(3,1)<<endl;
+	
+	  {
+    TGraph PVgraphIC;
+
+    for(int i=0;i<IC.size();i++){
+      PVgraphIC.SetPoint(i,IC[i][1],IC[i][2]);
+    }
+
+
+    TGraph PVgraphIE;
+
+    for(int i=0;i<IE.size();i++){
+      PVgraphIE.SetPoint(i,IE[i][1],IE[i][2]);
+    }
+
+
+    TGraph PVgraphAC;
+
+    for(int i=0;i<AC.size();i++){
+      PVgraphAC.SetPoint(i,AC[i][1],AC[i][2]);
+    }
+
+
+    TGraph PVgraphAE;
+
+    for(int i=0;i<AE.size();i++){
+      PVgraphAE.SetPoint(i,AE[i][1],AE[i][2]);
+    }
+
+
+    PVgraphIC.Draw();
+    PVgraphIE.Draw("same");
+    PVgraphAC.Draw("same");
+    PVgraphAE.Draw("same");
+
+    c1->SaveAs("PVgraph.pdf");
+
+
+  }
 
 	return 0;
 }
