6c8eb87ff07f98081873fe67ae59de0769ac8052
Diogo <diogo.s.simoes@tecnico.ulisboa.pt>
2021-03-25 16:26:55 +0000

    Calc.C

---
+++ Stirling/main/calc.C
@@ -0,0 +1,12 @@
+#include <cmath>
+#include "NewReader.h"
+#include "TGraph.h"
+#include "TCanvas.h"
+#include "TApplication.h"
+#include "LETintegral.h"
+
+int main()  {
+    double Vmin=140e-6;
+    double Vmax=278e-6;
+    return 0;
+}
