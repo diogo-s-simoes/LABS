a4b466fea91fcf23b523f94e8762b769c7599f2e
Diogo <diogo.s.simoes@tecnico.ulisboa.pt>
2021-03-25 16:36:36 +0000

    Calc.C+

--- Stirling/main/calc.C
+++ Stirling/main/calc.C
@@ -8,5 +8,24 @@
 int main()  {
     double Vmin=140e-6;
     double Vmax=278e-6;
+    double lProny=0.247;
+    double dEmb=6.0e-2;
+
+    Ub=3.632;
+    Ib=3.158;
+    dMb=0.559e-3;
+
+    Ut1=14.32;	
+    It1=12.6;
+    dMt1=0.595e-3;
+
+    Ut2=16.00;
+    It2=14.3;
+    dMt2=0.626e-3;
+
+    Ut3=17.59;
+    It3=15.6;
+    dMt3=0.621e-3;
+
     return 0;
 }
