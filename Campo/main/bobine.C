d64b98179745042c28ce923c1e1265e68599e2eb
Diogo <diogo.s.simoes@tecnico.ulisboa.pt>
2021-05-30 12:38:07 +0100

    efgviu

--- Campo/main/bobine.C
+++ Campo/main/bobine.C
@@ -111,7 +111,7 @@ int main(){
     TGraph Gfe;    Gar.SetTitle(""); Gfe.SetLineColor(kMagenta); Gfe.SetLineWidth(2); Gfe.SetMarkerStyle(16); Gfe.SetMarkerColor(kAzure+3);
 
     for(int i=0;i<Calib.GetLines();++i){
-        Gcalib.SetPoint(i,datac[i][0]/100,datac[i][1]/1000);
+        Gcalib.SetPoint(i,datac[i][0],datac[i][1]/1000);
     }
 
     double Rb=0.068; //m
@@ -127,7 +127,7 @@ int main(){
     Gcalib.Fit(Ffit);
 
     auto calib = [&](double *x,double *p=nullptr){
-        return m0*Nb*(x[0]-0.000715773)/(2*Rb*0.610303);
+        return m0*Nb*(x[0]-0.000715773)/(2*Rb*0.00610303);
     };
     TF1 *Fcalib = new TF1("FIT", calib, -10000,10000,0);
 
@@ -157,32 +157,111 @@ int main(){
     }
     
     double I=1;
+    double err=0;
+    //Bobine circular
+    //x=0 y=0
     auto ex = [&](double *x,double *p=nullptr){
         return p[0]*sin(x[0])/pow((Rb*Rb+p[0]*p[0]), 3./2.);
     };
-    TF1 *fex= new TF1("F", ex, -10000,10000,1);
-    //Bobine circular
+    TF1 *fex= new TF1("F", ex, -0.15,0.15,1);
     auto Bx = [&](double *x,double *p=nullptr){
         fex->SetParameter(0, x[0]);
-        Integrator B(0, 2*M_PI, *fex);
-        double Bvalue=0;
-        double err=0;
-        B.Simpson(10000, Bvalue, err);
+        double Bvalue=fex->Integral(0,2*M_PI);
         return m0*Nb*Rb*I/(4*M_PI)*Bvalue;
     };
-    TF1 *Bex= new TF1("F", Bx, -10000,10000,0);
+    TF1 *Bex= new TF1("F", Bx, -0.15,0.15,0);
 
     auto ez = [&](double *x,double *p=nullptr){
         return Rb/pow((Rb*Rb+p[0]*p[0]), 3./2.);
     };
-    TF1 *fez= new TF1("F", ez, -10000,10000,1);
+    TF1 *fez= new TF1("F", ez, -0.15,0.15,1);
     
     auto Bz = [&](double *x,double *p=nullptr){
         fez->SetParameter(0, x[0]);
         double Bvalue=fez->Integral(0,2*M_PI);
-        return 0.01*m0*Nb*Rb*I/(4*M_PI)*Bvalue;
+        return m0*Nb*Rb*I/(4*M_PI)*Bvalue;
     };
     TF1 *Bez= new TF1("F", Bz, -0.15,0.15,0);
+
+    //x=0.025 y=0
+    double xx=0.025; //m
+    auto ex2 = [&](double *x,double *p=nullptr){
+        return p[0]*sin(x[0])/pow((Rb*Rb+p[0]*p[0]+xx*xx-2*Rb*xx*sin(x[0])), 3./2.);
+    };
+    TF1 *fex2= new TF1("F", ex2, -0.15,0.15,1);
+    
+    auto Bx2 = [&](double *x,double *p=nullptr){
+        fex2->SetParameter(0, x[0]);
+        double Bvalue=fex2->Integral(0,2*M_PI);
+        return m0*Nb*Rb*I/(4*M_PI)*Bvalue;
+    };
+    TF1 *Bex2= new TF1("F", Bx2, -0.15,0.15,0);
+
+    auto ez2 = [&](double *x,double *p=nullptr){
+        return (Rb-xx*sin(x[0]))/pow((Rb*Rb+p[0]*p[0]+xx*xx-2*Rb*xx*sin(x[0])), 3./2.);
+    };
+    TF1 *fez2= new TF1("F", ez2, -0.15,0.15,1);
+
+
+    auto Bz2 = [&](double *x,double *p=nullptr){
+        fez2->SetParameter(0, x[0]);
+        double Bvalue=fez2->Integral(0,2*M_PI);
+        return m0*Nb*Rb*I/(4*M_PI)*Bvalue;
+    };
+    TF1 *Bez2= new TF1("F", Bz2, -0.15,0.15,0);
+
+    //y=0 z=0
+    double L=0;
+    auto exx =[&](double *x,double *p=nullptr){
+        return ((L/2)*sin(x[0]))/pow(Rb*Rb+p[0]*p[0]+(L/2)*(L/2)-2*p[0]*Rb*sin(x[0]), 3./2.);
+    };
+    TF1 *fexx= new TF1("F", exx, -0.15,0.15,1);
+    auto Bxx = [&](double *x,double *p=nullptr){
+        fexx->SetParameter(0, x[0]);
+        double Bvalue=fexx->Integral(0,2*M_PI);
+        return m0*Nb*Rb*I/(4*M_PI)*Bvalue;
+    };
+    TF1 *Bexx= new TF1("F", Bxx, -0.15,0.15,0);
+
+    auto ezx =[&](double *x,double *p=nullptr){
+        return (Rb-p[0]*sin(x[0]))/pow(Rb*Rb+p[0]*p[0]+(L/2)*(L/2)-2*p[0]*Rb*sin(x[0]),3./2.);
+    };
+    TF1 *fezx= new TF1("F", ezx, -0.15,0.15,1);
+    auto Bzx = [&](double *x,double *p=nullptr){
+        fezx->SetParameter(0, x[0]);
+        double Bvalue=fezx->Integral(0,2*M_PI);
+        return m0*Nb*Rb*I/(4*M_PI)*Bvalue;
+    };
+    TF1 *Bezx= new TF1("F", Bzx, -0.15,0.15,0);
+
+    //Bobines de Helmholtz
+    auto Bh1x=[&](double *x,double *p=nullptr){
+        return Bex->Eval(x[0]-L/2)+Bex->Eval(x[0]+L/2);
+    };
+    TF1 *Bh1ex= new TF1("F", Bh1x, -0.15,0.15,0);
+    auto Bh1z=[&](double *x,double *p=nullptr){
+        return Bez->Eval(x[0]-L/2)+Bez->Eval(x[0]+L/2);
+    };
+    TF1 *Bh1ez= new TF1("F", Bh1z, -0.15,0.15,0);
+
+    auto Bh2x=[&](double *x,double *p=nullptr){
+        return Bex2->Eval(x[0]-L/2)+Bex2->Eval(x[0]+L/2);
+    };
+    TF1 *Bh2ex= new TF1("F", Bh2x, -0.15,0.15,0);
+    auto Bh2z=[&](double *x,double *p=nullptr){
+        return Bez2->Eval(x[0]-L/2)+Bez2->Eval(x[0]+L/2);
+    };
+    TF1 *Bh2ez= new TF1("F", Bh2z, -0.15,0.15,0);
+    
+    L=dbH;
+    auto Bh3x=[&](double *x,double *p=nullptr){
+        return -Bexx->Eval(x[0])+Bexx->Eval(x[0]);
+    };
+    TF1 *Bh3ex= new TF1("F", Bh3x, -0.15,0.15,0);
+    auto Bh3z=[&](double *x,double *p=nullptr){
+        return -Bezx->Eval(x[0])+Bezx->Eval(x[0]);
+    };
+    TF1 *Bh3ez= new TF1("F", Bh3z, -0.15,0.15,0);
 
     TCanvas* c1 = new TCanvas();
     Gcalib.Draw("AP");
@@ -193,9 +272,11 @@ int main(){
     c1->SaveAs("bzz.png");
     c1->Clear();
     Gbzx.Draw("AP");
+    Bez2->Draw("SAME");
     c1->SaveAs("bzx.png");
     c1->Clear();
     Gbxx.Draw("AP");
+    Bezx->Draw("SAME");
     c1->SaveAs("bxx.png");
     c1->Clear();
     Ghzz.Draw("AP");
