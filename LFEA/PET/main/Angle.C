648bb58916604c439f529f920e832df32ef2559d
Diogo <diogo.s.simoes@tecnico.ulisboa.pt>
2021-11-03 15:28:45 +0000

    Ilbiob

--- LFEA/PET/main/Angle.C
+++ LFEA/PET/main/Angle.C
@@ -19,14 +19,26 @@ int main(){
     DataReader data("data/angulos.txt");
     double Nlines=data.GetLines();
 
-    TGraph* Graphy = new TGraph();
+    TGraphErrors* Graphy = new TGraphErrors();
+    Graphy->SetMarkerSize(2);
+    Graphy->SetMarkerColor(kGreen+2);
+    Graphy->SetMarkerStyle(kFullSquare);
+    Graphy->SetLineWidth(3);
+    Graphy->SetLineColor(kBlue-1);
+    Graphy->SetFillColor(1);
+    Graphy->SetFillStyle(3004);
+    Graphy->GetXaxis()->SetTitle("Angle (rad)");
+    Graphy->GetYaxis()->SetTitle("Coincidences [s^{-1}]");
+    Graphy->SetTitle("Correlac#tilde{a}o #hat{A}ngular");
 
     for (int i = 0; i<Nlines; ++i){
-        Graphy->SetPoint(i,atof(&(data.GetData()[i][0][0])),atof(&(data.GetData()[i][3][0])));
+        Graphy->SetPoint(i,M_PI/180.*atof(&(data.GetData()[i][0][0])),atof(&(data.GetData()[i][3][0]))/atof(&(data.GetData()[i][4][0]))-52./200.);
+        Graphy->SetPointError(i,0.1*M_PI/180,sqrt(atof(&(data.GetData()[i][3][0])))/atof(&(data.GetData()[i][4][0])));
     }
 
-    double r0 = 15.6;
-    double R = 5;
+    double r0 = 15.3;
+    double R = 5.67/2;
+
     auto l_integrand = [&](double *x,double *p=nullptr){
       return r0/pow(r0*r0+x[0]*x[0],3./2.)*2*sqrt(R*R-x[0]*x[0]);
     };
@@ -34,16 +46,40 @@ int main(){
 
 
     auto l_Sangle = [&](double *x,double *p=nullptr){
-      return p[0]*f_integrand->Integral(-R,r0*sqrt(1/(pow(cos(acos(r0/sqrt(r0*r0+R*R))-fabs(x[0])),2))-1));
+        if(fabs(x[0])<=acos(r0/sqrt(r0*r0+R*R))) 
+            return p[0]*f_integrand->Integral(-R,r0*sqrt(1/(pow(cos(acos(r0/sqrt(r0*r0+R*R))-fabs(x[0])),2))-1));
+        if(fabs(x[0])<=2*acos(r0/sqrt(r0*r0+R*R)))
+            return p[0]*(f_integrand->Integral(-R,R)-f_integrand->Integral(-R,r0*sqrt(1/(pow(cos(acos(r0/sqrt(r0*r0+R*R))-fabs(x[0])),2))-1)));
+        else return 0.;
     };
-    TF1* f_Sangle= new TF1("Solid_Angle", l_Sangle, -acos(r0/sqrt(r0*r0+R*R)),acos(r0/sqrt(r0*r0+R*R)),1);
+    TF1* f_Sangle= new TF1("Solid_Angle", l_Sangle, -2*acos(r0/sqrt(r0*r0+R*R)),2*acos(r0/sqrt(r0*r0+R*R)),1);
     f_Sangle->SetParameter(0,1);
+
+    cout<<2*acos(r0/sqrt(r0*r0+R*R))*180/M_PI<<endl;
 
     Graphy->Fit(f_Sangle);
 
     TCanvas* c1 = new TCanvas("","",1920,1080);
+    gStyle->SetOptStat(0);
+    gPad->SetGrid(1, 1);
+    gStyle->SetLegendBorderSize(0);
+    gStyle->SetOptFit(111);
+    gStyle -> SetStatBorderSize(5);
+    gStyle -> SetStatFontSize(0.2);
+    gStyle -> SetStatFont(62);
+    gStyle -> SetStatTextColor(1);
+    gStyle -> SetStatColor(6);
+    gStyle -> SetStatStyle(1);
+    gStyle -> SetStatX(0.85);
+    gStyle -> SetStatY(0.85);
+    gStyle -> SetStatW(0.1365);
+
     Graphy->Draw("AP");
+    //f_Sangle->Draw();
     c1->SaveAs("angle.png");
+
+    f_Sangle->SetParameter(0,1);
+    cout<<f_Sangle->Eval(0.)<<endl;
 
     return 0;
 }
---
+++ LFEA/PET/main/s2.C
@@ -0,0 +1,124 @@
+#include <cmath>
+#include "TGraph.h"
+#include "TCanvas.h"
+#include "TApplication.h"
+#include "TLegend.h"
+#include "TStyle.h"
+#include "TPad.h"
+#include "TF1.h"
+#include "TH1D.h"
+#include "NewReader.h"
+#include "LETintegral.h"
+#include "Spline3Interpolator.h"
+#include "TGraphErrors.h"
+#include "TLatex.h"
+#include "DataReader.h"
+
+int main(){
+
+    DataReader data1("data/delay.txt");
+    double Nlines1=data1.GetLines();
+
+    DataReader data2("data/tab2.txt");
+    double Nlines2=data2.GetLines();
+
+    TGraphErrors* G1 = new TGraphErrors();
+    G1->SetMarkerSize(2);
+    G1->SetMarkerColor(kGreen+2);
+    G1->SetMarkerStyle(kFullSquare);
+    G1->SetLineWidth(3);
+    G1->SetLineColor(kBlue-1);
+    G1->SetFillColor(1);
+    G1->SetFillStyle(3004);
+    G1->GetXaxis()->SetTitle("Janela de Aquisicao [ns]");
+    G1->GetYaxis()->SetTitle("Contagens");
+    G1->SetTitle("Janela Minima");
+
+    TGraphErrors* G2 = new TGraphErrors();
+    G2->SetMarkerSize(2);
+    G2->SetMarkerColor(kGreen+2);
+    G2->SetMarkerStyle(kFullSquare);
+    G2->SetLineWidth(3);
+    G2->SetLineColor(kBlue-1);
+    G2->SetFillColor(1);
+    G2->SetFillStyle(3004);
+    G2->GetXaxis()->SetTitle("Janela de Aquisicao [s]");
+    G2->GetYaxis()->SetTitle("Coincidências [s^{-1}]");
+    G2->SetTitle("Coincidencias Fortuitas");
+
+
+    double R1=0;
+    double R2=0;
+    double dR1=0;
+    double dR2=0;
+
+
+    for (int i = 0; i<Nlines1; ++i){
+        G1->SetPoint(i,48-atof(&(data1.GetData()[i][0][0])),atof(&(data1.GetData()[i][3][0])));
+        G1->SetPointError(i,8,sqrt(atof(&(data1.GetData()[i][3][0]))));
+    }
+    for (int i = 0; i<Nlines2-1; ++i){
+        G2->SetPoint(i,(1e-9)*atof(&(data2.GetData()[i][0][0])),atof(&(data2.GetData()[i][3][0]))/atof(&(data2.GetData()[i][4][0])));
+        G2->SetPointError(i,(1e-9)*atof(&(data2.GetData()[i][5][0])),sqrt(atof(&(data2.GetData()[i][3][0])))/atof(&(data2.GetData()[i][4][0])));
+        R1+=atof(&(data2.GetData()[i][1][0]))/atof(&(data2.GetData()[i][4][0]));
+        R2+=atof(&(data2.GetData()[i][2][0]))/atof(&(data2.GetData()[i][4][0]));
+    }
+    R1/=(Nlines2-1);
+    R2/=(Nlines2-1);
+    for (int i = 0; i<Nlines2-1; ++i){
+        dR1=pow(atof(&(data2.GetData()[i][1][0]))/atof(&(data2.GetData()[i][4][0]))-R1,2);
+        dR2=pow(atof(&(data2.GetData()[i][2][0]))/atof(&(data2.GetData()[i][4][0]))-R2,2);
+    }
+    dR1=sqrt(dR1/(Nlines2-1));
+    dR2=sqrt(dR2/(Nlines2-1));
+    double dRt=4*(dR1/R1+dR2/R2);
+
+    cout<<"R1= "<<R1<<"+-"<<dR1<<endl;
+    cout<<"R2= "<<R2<<"+-"<<dR2<<endl;
+    cout<<"Rt= "<<2*R1*R2<<"+-"<<dRt<<endl;
+
+
+    auto l_step = [&](double *x,double *p=nullptr){
+      return p[0]*erf(p[1]*(x[0]-p[2]))+p[3];
+    };
+    TF1* f_step= new TF1("STEP", l_step, -1e9,1e9,4);
+
+
+    auto l_linear = [&](double *x,double *p=nullptr){
+      return p[0]*x[0]+p[1];
+    };
+    TF1* f_linear= new TF1("LINEAR", l_linear, -1e9,1e9,2);
+
+    TCanvas* c1 = new TCanvas("","",1920,1080);
+    gStyle->SetOptStat(0);
+    gPad->SetGrid(1, 1);
+    gStyle->SetLegendBorderSize(0);
+    gStyle->SetOptFit(111);
+    gStyle -> SetStatBorderSize(5);
+    gStyle -> SetStatFontSize(0.2);
+    gStyle -> SetStatFont(62);
+    gStyle -> SetStatTextColor(1);
+    gStyle -> SetStatColor(6);
+    gStyle -> SetStatStyle(1);
+    gStyle -> SetStatX(0.85);
+    gStyle -> SetStatY(0.85);
+    gStyle -> SetStatW(0.1365);
+
+    f_step->SetParameter(0,1);
+    f_step->SetParameter(1,0.1);
+    f_step->SetParameter(2,1);
+    f_step->SetParameter(3,1);
+
+    G1->Fit(f_step);
+    G2->Fit(f_linear);
+
+    G1->Draw("AP");
+    c1->SaveAs("delay.png");
+    c1->Clear();
+
+    G2->Draw("AP");
+    c1->SaveAs("janela.png");
+    c1->Clear();
+
+    return 0;
+}
